74263bcf1e6a311beab275c7d1f131c8257ecdf9
davidkusmianto <davidkusmianto@gmail.com>
2021-02-10 13:51:24 +0700

    First Submit - Submission Tiga

--- app/src/main/java/com/dicoding/jpsubsatu/tvshow/TvshowViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.dicoding.jpsubsatu.tvshow
-
-import androidx.lifecycle.ViewModel
-import com.dicoding.jpsubsatu.data.TvshowEntity
-import com.dicoding.jpsubsatu.utils.DataDummy
-
-class TvshowViewModel : ViewModel() {
-    fun getTvshows() : List<TvshowEntity> = DataDummy.generateTvshows()
-}
